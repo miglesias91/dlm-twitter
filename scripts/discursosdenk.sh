17aea176e65306e7b07591e6a723e965ec7a52c6
EC2 Default User <ec2-user@ip-172-31-47-204.sa-east-1.compute.internal>
2021-03-14 01:24:15 +0000

    agregados scripts

---
+++ scripts/discursosdenk.sh
@@ -0,0 +1,4 @@
+!#/bin/sh
+source /home/ec2-user/repos/dlm-twitter/env/bin/activate
+cd /home/ec2-user/repos/dlm-twitter/
+env/bin/python3 ./twittero.py discursosdenk
---
+++ scripts/tn.sh
@@ -0,0 +1,5 @@
+!#/bin/sh
+source /home/ec2-user/repos/dlm-twitter/env/bin/activate
+cd /home/ec2-user/repos/dlm-twitter/
+ayer=$(date -d "yesterday" +"%Y%m%d")
+env/bin/python3 ./twittero.py dicenlosmedios ${ayer} todonoticias politica-economia-sociedad-internacional
